0a93d0766840f40ec80c4c019495a64dc37dcf98
Ebere Uzodufa <ebereuzodufa@gmail.com>
2020-06-29 22:36:36 +0100

    Adds contacts info

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  contactForm = this.fb.group({
+    firstname: ['', [Validators.required]],
+    lastname: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    phoneNumber: ['', [
+        Validators.required,
+        this.validatePhoneNumber
+      ]
+    ],
+    body: ['', [Validators.required]]
+  });
 
-  constructor() { }
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  get getContactFrom() {
+    return this.contactForm.controls;
+  }
 
   ngOnInit(): void {
   }
 
+  validatePhoneNumber(control: AbstractControl): {[key: string]: any} | null {
+    const regx = new RegExp(/(^[0]\d{10}$)|(^[\+]?[234]\d{12}$)/g);
+    const isNotValidPhone = regx.test(control.value);
+    return isNotValidPhone ? null : {isNotValidPhone: true};
+  }
+
+  onSubmit() {
+    if (this.contactForm.invalid) {
+      return;
+    }
+
+    console.log('form values', this.contactForm.value);
+  }
 }
