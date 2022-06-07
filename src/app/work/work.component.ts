8e4d68431ce09774581f744ce45f62dff983db97
Ebere Uzodufa <19553200+EbereUzodufa@users.noreply.github.com>
2022-06-07 22:18:07 +0100

    Update work.component.ts

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -14,7 +14,7 @@ export class WorkComponent implements OnInit {
       work_type: "Web Application",
       work_showcase:'https://res.cloudinary.com/dv1dxey5w/image/upload/v1593985462/Capture_jh3e6y.png',
       work_desc: "Sophies Bath and Body is a digital web and app-based purchasing platform for Sophies beauty products. Sophies bath and body website makes it easy for its customers to purchase products at any time, and as soon as new products are launched.",
-      url: 'https://sophiesbathandbody.com/'
+      url: 'http://stmbeautyandbath.com/'
     }
   ]
 
