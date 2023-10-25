71c41814552a9306b2f9c3c406c7fa445f1bdded
ebere <ebereuzodufa@gmail.com>
2023-10-25 23:37:10 +0100

    revert package name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tis-new'`, () => {
+  it(`should have as title 'tis'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tis-new');
+    expect(app.title).toEqual('tis');
   });
 
   it('should render title', () => {
@@ -25,7 +25,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'tis-new app is running!'
+      'tis app is running!'
     );
   });
 });
